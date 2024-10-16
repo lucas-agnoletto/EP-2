bbcb0e48b63e8e94981d837400f68d91ce4e29b6
lucas-agnoletto <lucasagnoletto07@gmail.com>
2024-10-16 09:18:15 -0300

    Resolução EX 1

---
+++ funcoes.py
@@ -0,0 +1,25 @@
+
+linha = 2
+coluna = 4
+orientacao = 'vertical'
+tamanho = 3
+
+
+def define_posicoes(linha, coluna, orientacao, tamanho):
+    
+    lista_posicoes = [[]]*tamanho
+    lista_posicoes[0] = [linha, coluna]
+
+    if orientacao == 'vertical':
+        i = 1
+        while(i < tamanho):
+            lista_posicoes[i] = [linha + i, coluna]
+            i += 1
+
+    if orientacao == 'horizontal':
+        i = 1
+        while(i < tamanho):
+            lista_posicoes[i] = [linha, coluna + i]
+            i += 1
+
+    return lista_posicoes
